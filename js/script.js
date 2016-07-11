e1f2e962e3882ced904899c6e751a054d112954e
Megan Coyle <meg@mcoyle.com>
2016-07-11 19:02:42 -0400

    Fix keypress issue for input views, need to correct for next card links

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // store alphabet
+var alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 // convert alphabet to clickable text
 
@@ -12,4 +13,9 @@
   // if matches, show word
   // else, draw Hangman
 
-  
+// drawing the Hangman
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.beginPath();
+ctx.arc(95,50,40,0,2*Math.PI);
+ctx.stroke();
