af584bd19cf0ea1ba36ad46c2e5f39908781248f
Megan Coyle <meg@mcoyle.com>
2016-07-11 17:47:23 -0400

    Add pseudo code

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// store alphabet
+
+// convert alphabet to clickable text
+
+// store word
+
+  // create placeholder _ for word
+  // way to compare word to guesses
+  // guesses = when user clicks on a specific letter
+
+// for each guess, check if it matches letter in word
+  // if matches, show word
+  // else, draw Hangman
+
+  
