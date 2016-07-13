9512206b8d197b8c60763aee3a5b5dca5e384384
Megan Coyle <meg@mcoyle.com>
2016-07-13 10:19:26 -0400

    Add logic for creating placeholder for word to guess

--- js/script.js
+++ js/script.js
@@ -3,12 +3,19 @@ var alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 
 // create buttons for alphabet
 for (i = 0; i < alphabet.length; i++) {
-  var alphabetBtn = document.getElementById('alphabet-buttons');
+  var alphabetBtn = document.getElementById("alphabet-buttons");
   alphabetBtn.innerHTML = alphabetBtn.innerHTML + '<li>' + alphabet[i] +'</li>';
 }
 
 // store word
 // var word = prompt("Player 1, pick a word!");
+var word = "pizza";
+
+// create placeholder _ for word
+for (i = 0; i < word.length; i++) {
+  var selectedWord = document.getElementById("selected-word");
+  selectedWord.innerHTML = selectedWord.innerHTML + '_';
+}
 
 // store a hint
 // if hint is used, an element is drawn
@@ -17,8 +24,6 @@ for (i = 0; i < alphabet.length; i++) {
 
 // convert alphabet to clickable text
 
-
-  // create placeholder _ for word
   // way to compare word to guesses
   // guesses = when user clicks on a specific letter
 
