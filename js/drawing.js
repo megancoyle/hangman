c261cd7d70e36301de3993a4010c983154e82f37
Megan Coyle <meg@mcoyle.com>
2016-07-18 08:35:29 -0400

    Remove drawing array

--- js/drawing.js
+++ js/drawing.js
@@ -82,5 +82,3 @@ function frame11() {
   rightLeg.lineTo(150, 180);
   rightLeg.stroke();
 }
-
-var drawing = [frame1, frame2, frame3, frame4, frame5, frame6, frame7, frame8, frame9, frame10, frame11];
