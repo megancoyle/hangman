47eb50b4872944afdac3bc449027f0a2ac792fde
Megan Coyle <meg@mcoyle.com>
2016-07-18 09:26:07 -0400

    Refactor frame functions

--- js/drawing.js
+++ js/drawing.js
@@ -4,41 +4,33 @@ var canvas = document.getElementById("myCanvas");
 // moveTo(x,y) - defines the starting point of the line
 // lineTo(x,y) - defines the ending point of the line
 
-function frame1(){
-  var platform1 = canvas.getContext("2d");
-  platform1.lineWidth=5;
-  platform1.strokeStyle="white";
-  platform1.moveTo(5, 290);
-  platform1.lineTo(210, 290);
-  platform1.stroke();
+function draw (moveTox, moveToy, lineTox, lineToy) {
+  var line = canvas.getContext("2d");
+  line.moveTo(moveTox, moveToy);
+  line.lineTo(lineTox, lineToy);
+  line.lineWidth=5;
+  line.strokeStyle="white";
+  line.stroke();
+}
+
+function frame1() {
+  draw(5, 290, 210, 290);
 }
 
 function frame2() {
-  var platform2 = canvas.getContext("2d");
-  platform2.moveTo(30, 6);
-  platform2.lineTo(30, 290);
-  platform2.stroke();
+  draw (30, 6, 30, 290);
 }
 
 function frame3() {
-  var platform3 = canvas.getContext("2d");
-  platform3.moveTo(27, 6);
-  platform3.lineTo(153, 6);
-  platform3.stroke();
+  draw (27, 6, 153, 6);
 }
 
 function frame4() {
-  var platform4 = canvas.getContext("2d");
-  platform4.moveTo(150, 6);
-  platform4.lineTo(150, 40);
-  platform4.stroke();
+  draw (150, 6, 150, 40);
 }
 
 function frame5() {
-  var platform5 = canvas.getContext("2d");
-  platform5.moveTo(30, 50);
-  platform5.lineTo(60, 6);
-  platform5.stroke();
+  draw (30, 50, 60, 6);
 }
 
 function frame6() {
@@ -49,36 +41,21 @@ function frame6() {
 }
 
 function frame7() {
-  var torso = canvas.getContext("2d");
-  torso.moveTo(150, 100);
-  torso.lineTo(150, 180);
-  torso.stroke();
+  draw (150, 100, 150, 180);
 }
 
 function frame8() {
-  var leftArm = canvas.getContext("2d");
-  leftArm.moveTo(100, 100);
-  leftArm.lineTo(150, 150);
-  leftArm.stroke();
+  draw (100, 100, 150, 150);
 }
 
 function frame9() {
-  var rightArm = canvas.getContext("2d");
-  rightArm.moveTo(150, 150);
-  rightArm.lineTo(200, 100);
-  rightArm.stroke();
+  draw (150, 150, 200, 100);
 }
 
 function frame10() {
-  var leftLeg = canvas.getContext("2d");
-  leftLeg.moveTo(100, 250);
-  leftLeg.lineTo(150, 180);
-  leftLeg.stroke();
+  draw (100, 250, 150, 180);
 }
 
 function frame11() {
-  var rightLeg = canvas.getContext("2d");
-  rightLeg.moveTo(200, 250);
-  rightLeg.lineTo(150, 180);
-  rightLeg.stroke();
+  draw (200, 250, 150, 180);
 }
