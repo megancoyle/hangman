fbc959512fb1e68eefa16c47be66efedc4e204f6
Megan Coyle <meg@mcoyle.com>
2016-07-28 11:32:22 -0400

    Convert array to string

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // store alphabet
-var alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
 var numberOfGuesses = 0;
 // store word
