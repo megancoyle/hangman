c2fad6ca3d5c24b6d203e256f8354760fe56871e
Megan Coyle <megan@mcoyle.com>
2018-09-03 21:37:19 -0400

    Begin to clean up functions

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
 # Hangman JavaScript Game
 
 ## Approach
+
 I decided to use the canvas element to create drawings that would render for each part of the hangman.
 
 ## User Stories
-* As a user, I should know when I guess incorrectly by seeing a counter for number of guesses remaining.
-* As a user, I should be able to know when I guess correctly by seeing letters appear for my guess.
-* As a user, I should be able to press a button/link to select my letter choice.
-* As a user, I should know when the game is over by receiving a 'game over' message.
-* As a user, I should be able to play again.
+
+- As a user, I should know when I guess incorrectly by seeing a counter for number of guesses remaining.
+- As a user, I should be able to know when I guess correctly by seeing letters appear for my guess.
+- As a user, I should be able to press a button/link to select my letter choice.
+- As a user, I should know when the game is over by receiving a 'game over' message.
+- As a user, I should be able to play again.
 
 [Play Hangman](https://megancoyle.github.io/hangman/)
 
 [![Hangman Screenshot](hangman.jpg)](https://megancoyle.github.io/hangman/)
 
 ## What's Next?
-* Refactor code
-* Clean up functions and variables
+
+- Refactor code
+- Make JS more modular/abstract
+- Clean up functions and variables
--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-// store alphabet
 var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-
+var alphabetBtns = document.getElementById("alphabet-buttons");
 var numberOfGuesses = 0;
-// store word
 var shuffleWordsArray = shuffle(words);
 var word = shuffleWordsArray.pop();
 var hiddenWord;
@@ -12,32 +10,49 @@ var container = document.getElementById("container");
 var guesses = [];
 var keepScore = [];
 var gamesRight = 0;
-// set limit for user
 var lives = 11;
 var score = document.getElementById("score");
 
-startGame();
+// event listener used for each specific alphabet button
+function getEventTarget(e) {
+  return e.target;
+}
 
-function startGame() {
-  // create buttons for alphabet
+function createButtons() {
   for (i = 0; i < alphabet.length; i++) {
-    var alphabetBtns = document.getElementById("alphabet-buttons");
     alphabetBtns.innerHTML =
       alphabetBtns.innerHTML + "<li>" + alphabet[i] + "</li>";
   }
+}
 
-  // create placeholder for word
+function createWordPlaceholder() {
   for (i = 0; i < word.length; i++) {
     selectedWord.innerHTML = selectedWord.innerHTML + "_";
   }
   hiddenWord = selectedWord.innerText;
+}
 
-  // event listener for each specific alphabet button
-  function getEventTarget(e) {
-    return e.target;
+function evaluateGuess() {
+  var guess = keepScore.pop();
+  if (word.indexOf(guess) > -1) {
+  } else if (lives <= 11 && lives > 0) {
+    lives--;
+    drawHangman();
+  } else {
+    // game over view
+    container.innerHTML =
+      "<h2>Game Over</h2><p>The correct word was: <span id='answer'></span></p><a id='play-again' href='#'>Play Again?</a>";
+    var answer = document.getElementById("answer");
+    answer.innerHTML = word;
+    var playAgain = document.getElementById("play-again");
+    playAgain.onclick = function(e) {
+      e.preventDefault();
+      location.reload();
+    };
   }
+}
 
-  // user guesses a letter by clicking on a letter button
+function guess() {
   alphabetBtns.onclick = function(event) {
     var target = getEventTarget(event);
     // make sure target clicked is li
@@ -58,6 +73,12 @@ function startGame() {
   };
 }
 
+function startGame() {
+  createButtons();
+  createWordPlaceholder();
+  guess();
+}
+
 // replacing an index of a string with a specific character
 String.prototype.replaceAt = function(index, character) {
   return (
@@ -71,7 +92,6 @@ function maskWord() {
   var regexp = new RegExp("[^" + guessString + "]", "g");
   var display = word.replace(regexp, "_");
   for (i = 0; i < word.length; i++) {
-    // turn guesses array into a string
     // update correct letter in masked word
     selectedWord.innerHTML = display;
     // if the right answer is guessed, let the user know
@@ -89,29 +109,10 @@ function maskWord() {
       }, 900);
     }
   }
-  // keep sore of games that are right - need to add this to local storage
+  // keep score of games that are right - need to add this to local storage
   if (word == display) {
     return gamesRight++;
   }
 }
 
-// on each frame, run animation of hangman
-function evaluateGuess() {
-  var guess = keepScore.pop();
-  if (word.indexOf(guess) > -1) {
-  } else if (lives <= 11 && lives > 0) {
-    lives--;
-    drawHangman();
-  } else {
-    // game over view
-    container.innerHTML =
-      "<h2>Game Over</h2><p>The correct word was: <span id='answer'></span></p><a id='play-again' href='#'>Play Again?</a>";
-    var answer = document.getElementById("answer");
-    answer.innerHTML = word;
-    var playAgain = document.getElementById("play-again");
-    playAgain.onclick = function(e) {
-      e.preventDefault();
-      location.reload();
-    };
-  }
-}
+startGame();
