734308a2e65e9947a49e58cdef4be20b63e24e2e
Megan Coyle <megan@mcoyle.com>
2018-09-23 22:50:14 -0400

    Clean up comments

--- js/script.js
+++ js/script.js
@@ -1,26 +1,28 @@
 "use strict";
 
-var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-var alphabetBtns = document.getElementById("alphabet-buttons");
-var numberOfGuesses = 0;
+// Uses words.js and utils.js
 var shuffleWordsArray = shuffle(words);
 var word = shuffleWordsArray.pop();
-var hiddenWord;
-var updateHiddenWord;
+
+// UI
+var alphabetBtns = document.getElementById("alphabet-buttons");
 var selectedWord = document.getElementById("selected-word");
 var container = document.getElementById("container");
+var score = document.getElementById("score");
+
+// Scoring/Game logic
+var numberOfGuesses = 0;
 var guesses = [];
 var keepScore = [];
 var gamesRight = 0;
 var lives = 11;
-var score = document.getElementById("score");
 
-// event listener used for each specific alphabet button
 function getEventTarget(e) {
   return e.target;
 }
 
 function createButtons() {
+  var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
   for (var i = 0; i < alphabet.length; i++) {
     alphabetBtns.innerHTML =
       alphabetBtns.innerHTML + "<li>" + alphabet[i] + "</li>";
@@ -31,19 +33,20 @@ function createWordPlaceholder() {
   for (var i = 0; i < word.length; i++) {
     selectedWord.innerHTML = selectedWord.innerHTML + "_";
   }
-  hiddenWord = selectedWord.innerText;
 }
 
 function evaluateGuess() {
   var guess = keepScore.pop();
+  var gameOverText =
+    "<h2>Game Over</h2><p>The correct word was: <span id='answer'></span></p><a id='play-again' href='#'>Play Again?</a>";
+
   if (word.indexOf(guess) > -1) {
   } else if (lives <= 11 && lives > 0) {
     lives--;
     drawHangman();
   } else {
     // game over view
-    container.innerHTML =
-      "<h2>Game Over</h2><p>The correct word was: <span id='answer'></span></p><a id='play-again' href='#'>Play Again?</a>";
+    container.innerHTML = gameOverText;
     var answer = document.getElementById("answer");
     answer.innerHTML = word;
     var playAgain = document.getElementById("play-again");
@@ -66,7 +69,6 @@ function guess() {
       guesses.push(guess);
       keepScore.push(guess);
 
-      // keep track of the number of guesses
       numberOfGuesses++;
       score.innerHTML = numberOfGuesses;
       maskWord();
@@ -81,26 +83,21 @@ function startGame() {
   guess();
 }
 
-// replacing an index of a string with a specific character
-String.prototype.replaceAt = function(index, character) {
-  return (
-    this.substr(0, index) + character + this.substr(index + character.length)
-  );
-};
-
 function maskWord() {
   // for each guess, check if it matches letter in word
   var guessString = guesses.join("");
   var regexp = new RegExp("[^" + guessString + "]", "g");
   var display = word.replace(regexp, "_");
+  var winnerText =
+    "<h2>You Got it Right! The word was <span id='answer'></span></h2><a id='play-again' href='#'>Play Again?</a>";
+
   for (var i = 0; i < word.length; i++) {
     // update correct letter in masked word
     selectedWord.innerHTML = display;
     // if the right answer is guessed, let the user know
     if (word == display) {
       setTimeout(function() {
-        container.innerHTML =
-          "<h2>You Got it Right! The word was <span id='answer'></span></h2><a id='play-again' href='#'>Play Again?</a>";
+        container.innerHTML = winnerText;
         var answer = document.getElementById("answer");
         answer.innerHTML = word;
         var playAgain = document.getElementById("play-again");
@@ -111,7 +108,7 @@ function maskWord() {
       }, 900);
     }
   }
-  // keep score of games that are right - need to add this to local storage
+
   if (word == display) {
     return gamesRight++;
   }
